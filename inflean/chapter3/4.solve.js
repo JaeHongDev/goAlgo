09568d908e1ec9816bc2ea04850affd30bdf8766
JaeHongDev <println@kakao.com>
2021-05-16 14:12:13 +0000

    20210516

--- inflean/chapter3/4.solve.js
+++ inflean/chapter3/4.solve.js
@@ -1,9 +1,31 @@
 const util = require("./util")
-module.exports = function(){
-    
-    return solution()
+module.exports = function () {
+    const s = "teachermode";
+    const c = "e"
+    return soluton(s, c)
 }
 
-function solution() {
+function soluton(str = "", c = "") {
+    const a = str.split('e')
+    console.log(a)
+    const answer = [];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === "") answer.push(0);
 
+        let cnt = 0;
+        for (let j = 0; j < a[i].length; j++) {
+            if (cnt <= i + a[i].length - cnt) {
+                answer.push(cnt)
+                console.log(a[i][j],i + a[i].length)
+                cnt++
+            }
+            
+            cnt++;
+        }
+
+        answer.push(0)
+        cnt = 1;
+    }
+    console.log(answer)
+    return 0;
 }
