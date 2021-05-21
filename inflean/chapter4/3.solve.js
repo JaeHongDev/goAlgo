073bb78bf878e59b84d3788165879259f41d9752
JaeHongDev <println@kakao.com>
2021-05-21 13:27:04 +0000

    20210521

--- inflean/chapter4/3.solve.js
+++ inflean/chapter4/3.solve.js
@@ -1,9 +1,26 @@
 const util = require("./util")
-module.exports = function(){
-    
-    return solution()
+module.exports = function () {
+    const [N, M] = [3, 4];
+
+    const students = [
+        util.stringToIntArray("3 4 1 2 "),
+        util.stringToIntArray("4 3 2 1"),
+        util.stringToIntArray("3 1 4 2"),
+    ]
+
+    return solution(N, M, students)
 }
 
-function solution() {
+function solution(N = 3, M = 4, students = [[]]) {
+    const answers = [];
+    for (let i = 0; i < students.length - 1; i++) {
+        for (let j = 0; j < students[i].length - 1; j++) {
+            for (let k = j + 1; k < students[i].length; k++) {
+                
+            }
+        }
+
+    }
+
     return 0;
 }
