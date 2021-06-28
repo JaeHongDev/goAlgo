278dad2057dc812e6108a7f4596bc51b361d6c86
JaeHongDev <println@kakao.com>
2021-06-28 13:41:55 +0000

    20210628

--- inflean/chapter9/5.solve.js
+++ inflean/chapter9/5.solve.js
@@ -4,12 +4,36 @@ const stringToSplitArray = (string = "") => string.trimEnd().split("")
 
 module.exports = function () {
 
-    return solution()
+    const p = 5; 
+    const k = 14;
+    return solution(p,k)
 }
 
-function solution() {
-    return 0;
+function solution(p,k) {
+    let answer = 0;
+    const ch = new Array(10001).fill(0);
+    const dis = new Array(10001).fill(0);
+
+    const queue = [];
+    queue.push(p);
+    ch[p] = 1; 
+    dis[p] = 0;
+
+    while(queue.length) {
+        let x = queue.shift();
+        for(nx of [x-1,x+1,x+5]){
+            if(nx === k) return dis[x] =1; 
+            if(nx > 0 && nx <= 10000 && ch[nx] === 0) {
+                ch[nx] =1;
+                queue.push(nx);
+                dis[nx]=dis[x] +1;
+            }
+        }
+        break;
+    }
+    return answer;
 }
+
 
 
 
