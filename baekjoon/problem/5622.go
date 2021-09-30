f2023b3cb578e40ff4ef8367224ce39c733c5754
JaeHongDev <println@kakao.com>
2021-09-30 11:03:31 +0000

    20210930

--- baekjoon/problem/5622.go
+++ baekjoon/problem/5622.go
@@ -1,34 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	//inputs
-	var inputs []string
-	inputs = make([]string, 2)
-	inputs[0] = "WA"
-	inputs[1] = "UNUCIC"
+	// var inputs []string
+	// inputs = make([]string, 2)
+	// inputs[0] = "WA"
+	// inputs[1] = "UNUCIC"
 
-	// Scanner := bufio.NewScanner(os.Stdin)
-	// Scanner.Scan()
-	// inputString := Scanner.Text()
-	// fmt.Println(inputString)
+	Scanner := bufio.NewScanner(os.Stdin)
+	Scanner.Scan()
+	inputString := Scanner.Text()
+	//fmt.Println(inputString)
+
+	inputCase := inputString
+
 	wordDB := make(map[int]int)
-	cnt := 0
-	index := 2
-	for i := 65; i < 95; i++ {
-		wordDB[i] = index
-		cnt++
+	wordDB[65] = 3
+	wordDB[66] = 3
+	wordDB[67] = 3
 
-		if cnt%3 == 0 {
-			cnt = 0
-			index++
-		}
-	}
-	wordDB[96] = index - 1
+	wordDB[68] = 4
+	wordDB[69] = 4
+	wordDB[70] = 4
 
-	for i := 0; i < len(inputs[0]); i++ {
-		inputs
+	wordDB[71] = 5
+	wordDB[72] = 5
+	wordDB[73] = 5
+
+	wordDB[74] = 6
+	wordDB[75] = 6
+	wordDB[76] = 6
+
+	wordDB[77] = 7
+	wordDB[78] = 7
+	wordDB[79] = 7
+
+	wordDB[80] = 8
+	wordDB[81] = 8
+	wordDB[82] = 8
+	wordDB[83] = 8
+
+	wordDB[84] = 9
+	wordDB[85] = 9
+	wordDB[86] = 9
+
+	wordDB[87] = 10
+	wordDB[88] = 10
+	wordDB[89] = 10
+	wordDB[90] = 10
+
+	// wordDB[92] = 3
+	// wordDB[93] = 3
+	// wordDB[94] = 3
+	// wordDB[95] = 3
+	// wordDB[] = 3
+	// wordDB[96] = index - 1
+	// wordDB[49] = 1
+	answer := 0
+	for i := 0; i < len(inputCase); i++ {
+		//fmt.Println(inputCase[i])
+		//fmt.Println(reflect.TypeOf(inputCase[i]))
+		answer += wordDB[int(inputCase[i])]
 	}
-	fmt.Println(wordDB)
+
+	// for key, value := range wordDB {
+	// 	fmt.Printf("%c %d \n", key, value)
+	// }
+	fmt.Println(answer)
 }
