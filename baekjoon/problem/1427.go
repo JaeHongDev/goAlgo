7f816c201ab235839554fae5edc101202028ae3c
JaehongDev <println@kakao.com>
2021-12-14 22:03:16 +0900

    1427 소트인사이드

---
+++ baekjoon/problem/1427.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	caseNumber := scanner.Text()
+
+	caseNumberSize := len(caseNumber) // 배열의 길이
+
+	unsortedArr := make([]int, caseNumberSize)
+
+	for i := 0; i < caseNumberSize; i++ {
+		item, _ := strconv.Atoi(string(caseNumber[i]))
+		unsortedArr[i] = item
+	}
+
+	sortedArr := QuickSort(unsortedArr)
+	answer := strings.Trim(strings.Replace(fmt.Sprint(sortedArr), " ", "", -1), "[]")
+	fmt.Println(answer)
+
+}
+
+func QuickSort(unsortedArray []int) []int {
+
+	if len(unsortedArray) < 2 {
+		return unsortedArray
+	}
+
+	left, right := 0, len(unsortedArray)-1
+
+	pivot := rand.Int() % len(unsortedArray)
+
+	unsortedArray[pivot], unsortedArray[right] = unsortedArray[right], unsortedArray[pivot]
+
+	for i := range unsortedArray {
+		if unsortedArray[i] > unsortedArray[right] {
+			unsortedArray[left], unsortedArray[i] = unsortedArray[i], unsortedArray[left]
+			left++
+		}
+
+	}
+
+	unsortedArray[left], unsortedArray[right] = unsortedArray[right], unsortedArray[left]
+
+	QuickSort(unsortedArray[:left])
+	QuickSort(unsortedArray[left+1:])
+
+	return unsortedArray
+}
