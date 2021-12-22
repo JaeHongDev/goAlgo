af2e7c891ad91d20d07bb2b0450b202e58e55049
JaehongDev <println@kakao.com>
2021-12-22 20:47:23 +0900

    15650 백트래킹2

--- baekjoon/js/15650.js
+++ baekjoon/js/15650.js
@@ -3,28 +3,23 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
 const [n, m] = input[0].split(" ");
-
 function solution(last, size) {
   const check = new Array(size).fill(false);
-
-  const dfs = (sequence=[]) => {
-    const check_size = check.length;
-    console.log(1)
-    // base case
-    if(sequence.length == size){
+  const dfs = (level = 0, sequence = []) => {
+    if (level == last+1 ) {
+      if (sequence.length == size) {
         console.log(sequence.toString().replace(/\,/g,' '));
-        return ;
+
+      }
+      return;
     }
-    for(let i = 0;i<last;i++){
-        if(!check[i]){
-            check[i] = true
-            sequence.push(i+1)
-            dfs(sequence)
-            check[i] = false
-            sequence.pop()
-        }
-    }
+    sequence.push(level);
+    check[level] = true;
+    dfs(level + 1, sequence);
+    sequence.pop();
+    check[level] = false;
+    dfs(level + 1, sequence);
   };
-  dfs(0,[])
+  dfs(1, []);
 }
 solution(Number(n), Number(m));
