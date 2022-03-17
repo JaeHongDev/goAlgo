69b428ec9d9540b5d35d8300ede2b4cb890a1bd9
JaehongDev <println@kakao.com>
2022-03-17 14:08:32 +0900

    백준 10844

---
+++ baekjoon/js/10844.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let number = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((item) => parseInt(item))[0];
+  
+  // dp[n][i] = n자리 수이면서 i로 끝나는 수
+  const dp = Array.from(new Array(number + 1), () => new Array(10));
+  
+  dp[1] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+  dp[2] = [1, 1, 2, 2, 2, 2, 2, 2, 2, 1];
+  
+  for (let n = 3; n <= number; n++) {
+    for (let i = 0; i < 10; i++) {
+      if (i === 0) {
+        dp[n][i] = dp[n - 1][i + 1] % 1000000000;
+      } else if (i >= 1 && i <= 8) {
+        dp[n][i] = (dp[n - 1][i - 1] + dp[n - 1][i + 1]) % 1000000000;
+      } else {
+        dp[n][i] = dp[n - 1][i - 1] % 1000000000;
+      }
+    }
+  }
+  let sum = 0;
+  for (let j = 0; j < 10; j++) {
+    sum += dp[number][j];
+  }
+  
+  console.log(sum % 1000000000);
