c0880856917cb1ae26946f5fa2ba0ef12396d606
JaehongDev <println@kakao.com>
2022-04-12 19:43:47 +0900

    11047 운영체제별 파일 결과 차이 확인

--- baekjoon/js/11047.js
+++ baekjoon/js/11047.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
 
 const [inputNumber, ...coins] = input;
 
@@ -8,7 +12,7 @@ let sum = Number(inputNumber.split(" ")[1]);
 let cnt = 0;
 for (let i = coins.length - 1; i >= 0; i--) {
   if (sum >= coins[i]) {
-    cnt += parseInt(sum / coins[i]);
+    cnt += Math.floor(sum / coins[i]);
     sum %= coins[i];
   }
 }
