40668b89845a0dfd2bbc6dd5fd71f23cd6068761
JaehongDev <println@kakao.com>
2022-04-27 08:45:19 +0900

    1935 후위 표현식2

--- baekjoon/js/202204/1935.js
+++ baekjoon/js/202204/1935.js
@@ -4,46 +4,40 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [casesStr, operationStr,...numbersStr] = input;
 
 const numbers = numbersStr.map(Number);
-
 const convertNum = (s = '') => numbers[s.charCodeAt() - 65];
-
 const stack = [];
-let answer = 0;
-let temp = 0;
-for (let s of operationStr) {
-    if (s.charCodeAt >= 65) {
+const hashTable = new Map();
+let cnt = 0;
+for (let s of operationStr.trim()) {
+    if (s.charCodeAt() >= 65) {
+        hashTable.has(s) ? "" : hashTable.set(s, convertNum(s));
         stack.push(s);
         continue;
     }
-    let p1, p2;
-    if (stack.length === 1) {
-        p1 = stack.pop();
-    } else {
-        p1 = stack.pop();
-        p2 = stack.pop();
-    }
-    if (s === "*") {
-        if (stack.length == 1) {
-            answer = answer * convertNum(p1)
-            continue;
-        }
-        answer = convertNum(p2) * convertNum(p1);
+    let p1 = hashTable.get(stack.pop());
+    let p2 = hashTable.get(stack.pop());
+    stack.push(cnt);
+    if (s === "+") {
+        hashTable.set(cnt, p1 + p2);
     }
     if (s === "-") {
-        if (stack.length == 1) {
-            answer = answer - convertNum(p1)
-            continue;
-        }
-        answer = convertNum(p2) - convertNum(p1);
+        hashTable.set(cnt, p2 - p1); 
     }
-
-    if (s === "+") {
-        if (stack.length == 1) {
-            answer = answer + convertNum(p1)
-            continue;
-        }
-        answer = convertNum(p2) + convertNum(p1);
+    if (s === "*") {
+        hashTable.set(cnt, p2 * p1);
     }
-
+    if (s === "/") {
+        hashTable.set(cnt, p2 / p1);
+    }
+    cnt++;
 }
-console.log(stack);
+console.log(hashTable.get(stack.pop()).toFixed(2));
+
+// stack
+/*
+ A B C
+ A 0 
+ 1 D E
+ 1 2
+ 3
+ */
