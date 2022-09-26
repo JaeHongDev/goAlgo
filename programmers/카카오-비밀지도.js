4a1d6bdf09f2bf41809ce43f07cf9de1a6507676
artisan <73726591+JaeHongDev@users.noreply.github.com>
2022-09-26 18:51:48 +0900

    Update 카카오-비밀지도.js

--- programmers/카카오-비밀지도.js
+++ programmers/카카오-비밀지도.js
@@ -1,17 +1,10 @@
 function solution(n, arr1, arr2) {
-    var answer = [];
-    const a = arr1[0].toString(2)
-   	const arrMap1 = arr1.map(i=>i.toString(2));
+    const arrMap1 = arr1.map(i=>i.toString(2));
     const arrMap2 = arr2.map(i=>i.toString(2));
     
-    for(let i = 0 ; i < n ; i++ ) {
-        let s1 = "0".repeat( n - arrMap1[i].length).concat(arrMap1[i]).split("");
-	    let s2 = "0".repeat( n - arrMap2[i].length).concat(arrMap2[i]).split("");
-       	answer.push(s1.map((s,index)=>{
-            if(s2[index] === "1" || s === "1") return "#"
-            return " ";
-        }).join(""))
-        
-    }
-    return answer;
+    return arr1.reduce((pre,cal,index)=> {
+    	let s1 = "0".repeat( n - arrMap1[index].length).concat(arrMap1[index]).split("");
+	let s2 = "0".repeat( n - arrMap2[index].length).concat(arrMap2[index]).split("");
+	return [...pre, s1.map((s,index) => s2[index] === "1" || s === s1[index] === "1" ? "#" : " ")] 
+    }).join("")
 }
