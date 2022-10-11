2755ca8e444c37351d2403edea4c2a920e942df1
JaehongDev <println@kakao.com>
2022-10-11 10:20:38 +0900

    chapter6 solve2

--- inflearnJava/solve6/C6_2.java
+++ inflearnJava/solve6/C6_2.java
@@ -17,8 +17,8 @@ public class C6_2 {
     }
 
     private int[] solution(int[] arr, int size) {
-        for(int i = 0 ; i < size ;i++){
-            for(int j= i; j <size-i-1;j++) {
+        for(int i = 0 ; i < size-1 ;i++){
+            for(int j= 0; j <size-i-1;j++) {
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
