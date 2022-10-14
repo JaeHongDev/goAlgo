c196ca0e2b884bca2d488a4ab5137a003c579a36
JaehongDev <println@kakao.com>
2022-10-14 17:00:39 +0900

    chapter6 solve10

---
+++ inflearnJava/solve5/C5_8.java
@@ -0,0 +1,26 @@
+class Person{
+    int index;
+    int percent;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
+
+    public Person(int index, int percent) {
+        this.index   = index;
+        this.percent = percent;
+    }
+}
+public class C5_8 {
+
+    public static void main(String[]args){
+
+    }
+
+}
+
+
---
+++ inflearnJava/solve6/C6_10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class C6_10 {
+    public static void main(String[] args){
+       C6_10 T = new C6_10();
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        for(int i = 0 ; i < n ; i++) arr[i] = scanner.nextInt();
+    }
+}
