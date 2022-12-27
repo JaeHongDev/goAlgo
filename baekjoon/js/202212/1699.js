fa3762e71a87cf3f0c7d81dfda119e0fc1553d84
JaehongDev <println@kakao.com>
2022-12-27 19:25:14 +0900

    feature(1699) : 백준 문제명[제곱합SILVER2 정답봄]

---
+++ baekjoon/js/202212/1699.js
@@ -0,0 +1,23 @@
+
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input= Number(fs
+    .readFileSync(filePath)
+    .toString());
+console.log(solution(input));
+
+/**
+ * @param {number} input
+ */
+function solution(input){
+    const cache = new Array(input+1).fill(0).map((_,index)=> index);
+
+    cache[1] = 1;
+    for(let i = 1; i<=input;i++){
+        for(let j=1; j**2 <=i; j++){
+            cache[i] = Math.min(cache[i],cache[i- j**2] +1)
+        }
+    }
+    return cache[input];
+}
